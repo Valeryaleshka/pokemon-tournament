4f8170ad26db88d59bfe595a136095f89a194ab4
Valery Aleshka <valeryaleshka@gmail.com>
2025-10-31 01:47:54 -0700

    Pokemon Provider: Fixed Http Type

--- src/app/shared/providers/pokemon.provider.ts
+++ src/app/shared/providers/pokemon.provider.ts
@@ -10,9 +10,8 @@ import {map} from 'rxjs/operators';
 export class PokemonProvider {
   http = inject(HttpClient);
 
-
   public getSinglePokemon(id: number): Observable<IPokemon> {
-    return this.http.get<Welcome>(`https://pokeapi.co/api/v2/pokemon/${id}`).pipe(map((data) => {
+    return this.http.get<PokermonApiResponse>(`https://pokeapi.co/api/v2/pokemon/${id}`).pipe(map((data) => {
       return {
         id: data.id,
         name: data.name,
@@ -28,197 +27,24 @@ export class PokemonProvider {
   }
 }
 
-export interface Welcome {
-  abilities: Ability[];
+export interface PokermonApiResponse {
   baseExperience: number;
-  cries: Cries;
-  forms: Species[];
-  gameIndices: GameIndex[];
-  height: number;
-  held_items: HeldItem[];
   id: number;
-  is_default: boolean;
-  location_area_encounters: string;
-  moves: Move[];
   name: string;
-  order: number;
-  past_abilities: PastAbility[];
-  past_types: any[];
-  species: Species;
   sprites: Sprites;
-  stats: Stat[];
   types: Type[];
-  weight: number;
-}
-
-export interface Ability {
-  ability: Species | null;
-  is_hidden: boolean;
-  slot: number;
-}
-
-export interface Species {
-  name: PokemonType;
-  url: string;
-}
-
-export interface Cries {
-  latest: string;
-  legacy: string;
-}
-
-export interface GameIndex {
-  game_index: number;
-  version: Species;
-}
-
-export interface HeldItem {
-  item: Species;
-  version_details: VersionDetail[];
-}
-
-export interface VersionDetail {
-  rarity: number;
-  version: Species;
-}
-
-export interface Move {
-  move: Species;
-  version_group_details: VersionGroupDetail[];
-}
-
-export interface VersionGroupDetail {
-  level_learned_at: number;
-  move_learn_method: Species;
-  order: number | null;
-  version_group: Species;
-}
-
-export interface PastAbility {
-  abilities: Ability[];
-  generation: Species;
-}
-
-export interface GenerationV {
-  "black-white": Sprites;
-}
-
-export interface GenerationIv {
-  "diamond-pearl": Sprites;
-  "heartgold-soulsilver": Sprites;
-  platinum: Sprites;
-}
-
-export interface Versions {
-  "generation-i": GenerationI;
-  "generation-ii": GenerationIi;
-  "generation-iii": GenerationIii;
-  "generation-iv": GenerationIv;
-  "generation-v": GenerationV;
-  "generation-vi": { [key: string]: Home };
-  "generation-vii": GenerationVii;
-  "generation-viii": GenerationViii;
-}
-
-export interface Other {
-  dream_world: DreamWorld;
-  home: Home;
-  "official-artwork": OfficialArtwork;
-  showdown: Sprites;
 }
 
 export interface Sprites {
-  backDefault: string;
-  back_female: null | string;
-  back_shiny: string;
-  back_shiny_female: null | string;
   frontDefault: string;
-  front_female: null | string;
-  front_shiny: string;
-  front_shiny_female: null | string;
-  other?: Other;
-  versions?: Versions;
-  animated?: Sprites;
-}
-
-export interface GenerationI {
-  "red-blue": RedBlue;
-  yellow: RedBlue;
-}
-
-export interface RedBlue {
-  back_default: string;
-  back_gray: string;
-  back_transparent: string;
-  front_default: string;
-  front_gray: string;
-  front_transparent: string;
-}
-
-export interface GenerationIi {
-  crystal: Crystal;
-  gold: Gold;
-  silver: Gold;
-}
-
-export interface Crystal {
-  back_default: string;
-  back_shiny: string;
-  back_shiny_transparent: string;
-  back_transparent: string;
-  front_default: string;
-  front_shiny: string;
-  front_shiny_transparent: string;
-  front_transparent: string;
-}
-
-export interface Gold {
-  back_default: string;
-  back_shiny: string;
-  front_default: string;
-  front_shiny: string;
-  front_transparent?: string;
-}
-
-export interface GenerationIii {
-  emerald: OfficialArtwork;
-  "firered-leafgreen": Gold;
-  "ruby-sapphire": Gold;
-}
-
-export interface OfficialArtwork {
-  front_default: string;
-  front_shiny: string;
-}
-
-export interface Home {
-  front_default: string;
-  front_female: null | string;
-  front_shiny: string;
-  front_shiny_female: null | string;
-}
-
-export interface GenerationVii {
-  icons: DreamWorld;
-  "ultra-sun-ultra-moon": Home;
-}
-
-export interface DreamWorld {
-  front_default: string;
-  front_female: null;
-}
-
-export interface GenerationViii {
-  icons: DreamWorld;
-}
-
-export interface Stat {
-  base_stat: number;
-  effort: number;
-  stat: Species;
 }
 
 export interface Type {
   slot: number;
   type: Species;
+}
+
+export interface Species {
+  name: PokemonType;
+  url: string;
 }
